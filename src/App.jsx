f63af6499e6b45aa69b407e8ea8eb966a25d13af
Ravi <lcs2020043@iiitl.ac.in>
2024-01-08 22:18:30 +0530

    Create project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null, // null means adding "new project"
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        id: Math.random(),
+        ...projectData,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAddProject={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+  console.log(projectState);
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
+      {content}
     </main>
   );
 }
